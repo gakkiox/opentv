baa9c9924b5979295f0838b3e4185d665fa1a979
ikux <iamaabb@163.com>
2023-09-20 22:19:45 +0800

    添加背景图片

--- pages/home.js
+++ pages/home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Text, View, Button, StyleSheet, ScrollView, Image } from "react-native";
-import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon from 'react-native-vector-icons/Feather';
 
 class Home extends React.Component {
   constructor(props) {
@@ -12,47 +12,58 @@ class Home extends React.Component {
   render() {
     let { list } = this.state
     return (
-      <View style={[styles.container]}>
-        <View style={[styles.head]}>
-          <View style={[styles.headBtn]}>
-            <Button title="电视剧" onPress={() => { }} />
-          </View>
-          <View style={[styles.headBtn]}>
-            <Button title="电影" onPress={() => { }} />
-          </View>
-          <View style={[styles.headBtn]}>
-            <Button title="设置" onPress={() => { }} />
-            <Icon name="rocket" size={30} color="#900" />
-          </View>
-        </View>
-        <View style={{ flex: 1, display: "flex", flexDirection: "row" }}>
-          <View style={{ width: 100, height: "100%" }}>
-            <ScrollView >
-              {list.map((item, index) => {
-                return (
-                  <View style={{}} key={index}>
-                    <Button title={item.name} onPress={() => { }} />
-                  </View>
-                )
-              })}
-            </ScrollView>
+      <View style={{ height: "100%", width: "100%", position: "relative", backgroundColor: "black" }}>
+        <View style={[styles.container]}>
+          <View style={[styles.head]}>
+            <View style={[styles.headBtn]}>
+              <Button title="电视剧" onPress={() => { }} />
+            </View>
+            <View style={[styles.headBtn]}>
+              <Button title="电影" onPress={() => { }} />
+            </View>
+            <View style={[styles.headBtn]}>
+              <Icon.Button
+                name="settings"
+                backgroundColor="#3b5998"
+                onPress={this.loginWithFacebook}
+              >
+                设置
+              </Icon.Button>
 
+            </View>
           </View>
-          <View style={{ flex: 1, display: "flex", flexDirection: "row", paddingHorizontal: 10 }}>
-            <ScrollView style={{ height: "100%", width: "100%", }}>
-              <View style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
+          <View style={{ flex: 1, display: "flex", flexDirection: "row" }}>
+            <View style={{ width: 100, height: "100%" }}>
+              <ScrollView showsVerticalScrollIndicator={false}>
                 {list.map((item, index) => {
                   return (
-                    <View style={{ marginRight: 10, marginBottom: 10 }} key={index}>
-                      <Image style={{ width: 180, height: 250, marginBottom: 10 }} source={{ uri: "https://img.wemp4.com/vod/c/650904f023c21.jpg" }} />
-                      <Button title={item.name} onPress={ ()=>this.props.navigation.navigate("Detail")} />
+                    <View style={{}} key={index}>
+                      <Button title={item.name} onPress={() => { }} />
                     </View>
                   )
                 })}
-              </View>
+              </ScrollView>
 
-            </ScrollView>
+            </View>
+            <View style={{ flex: 1, display: "flex", flexDirection: "row", paddingHorizontal: 10 }}>
+              <ScrollView style={{ height: "100%", width: "100%", }} showsVerticalScrollIndicator={false}>
+                <View style={{ display: "flex", flexDirection: "row", flexWrap: "wrap", justifyContent: "space-around" }}>
+                  {list.map((item, index) => {
+                    return (
+                      <View style={{ marginRight: 10, marginBottom: 10 }} key={index}>
+                        <Image style={{ width: 180, height: 250, marginBottom: 10 }} source={{ uri: "https://img.wemp4.com/vod/c/650904f023c21.jpg" }} />
+                        <Button title={item.name} onPress={() => this.props.navigation.navigate("Detail")} />
+                      </View>
+                    )
+                  })}
+                </View>
+
+              </ScrollView>
+            </View>
           </View>
+        </View>
+        <View style={[styles.fullScreen, { zIndex: 1, }]}>
+          <Image style={{ width: "100%", height: "100%", opacity: 0.4 }} source={require("../assets/home.jpg")} />
         </View>
       </View>
     )
@@ -60,7 +71,15 @@ class Home extends React.Component {
 }
 var styles = StyleSheet.create({
   container: {
-    height: "100%", width: "100%", display: "flex", flexDirection: "column"
+    height: "100%", width: "100%", display: "flex", flexDirection: "column",
+    position: "relative", zIndex: 10
+  },
+  fullScreen: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
   },
   headBtn: {
     marginHorizontal: 10
